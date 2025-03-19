317d7b256f19dfdcfe335c4b597971915c24557a
Ritik Shekhar Parida <ritikshekharparida2002@gmail.com>
2025-03-19 10:56:24 +0530

    add captain service with authentication, user management, and gateway integration

--- captain/middleware/authMiddleware.js
+++ captain/middleware/authMiddleware.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
-const userModel = require("../models/user.model");
-const blacklisttokenModel = require("../models/blacklisttoken.model");
+const captainModel = require("../models/captain.model");
 
-module.exports.userAuth = async (req, res, next) => {
+module.exports.captainAuth = async (req, res, next) => {
   try {
     const token = req.cookies.token || req.headers.authorization.split(" "[1]);
 
@@ -10,17 +9,15 @@ module.exports.userAuth = async (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const isBlacklisted = await blacklisttokenModel.find({token})
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await userModel.findById(decoded.id);
+    const captain = await captainModel.findById(decoded.id);
 
-    if (!user) {
+    if (!captain) {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    req.user = user;
+    req.captain = captain;
 
     next();
   } catch (error) {
